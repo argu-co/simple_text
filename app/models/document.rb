fcea880b28478de1bb13ae3b3cbbf63e3cfa72bc
Fletcher91 <thomvankalkeren@gmail.com>
2014-12-09 11:58:05 +0100

    [FIX] Bug where to_html wasn't accessible

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,6 +3,7 @@ class Document < ActiveRecord::Base
   validates_length_of :title, minimum: 4, maximum: 100
 
 
+  public
   def to_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, {tables: true, fenced_code_blocks: false, no_styles: true, escape_html: true}).render(contents)
   end
