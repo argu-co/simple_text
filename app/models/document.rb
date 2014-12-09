cb836ae28a50a83f65ede35771d04dcbfe3c9159
Fletcher91 <thomvankalkeren@gmail.com>
2014-12-09 11:44:14 +0100

    Added to_s function and reflect changes in the documentation

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,8 @@ class Document < ActiveRecord::Base
   validates_length_of :name, minimum: 4, maximum: 100
   validates_length_of :title, minimum: 4, maximum: 100
 
+
+  def to_s
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, {tables: true, fenced_code_blocks: false, no_styles: true, escape_html: true}).render(contents)
+  end
 end
